e6a34e20960581004d827a3116d89c77e58a2a59
Timo Erkkilä <timo.erkkila@gmail.com>
2017-08-20 14:28:50 +0300

    First commit

---
+++ numbers.py
@@ -0,0 +1,6 @@
+iidaAge = 100
+age = 56
+
+print("Hi, my name is Aino. I am {age} years old! Next year I will be {nextAge}.i live with my family in tampere.".format(age=age, nextAge=age+1))
+print("my little sister is {iidaAge} years old".format(iidaAge=iidaAge))
+print("Together we are {combinedAge} years old.".format(combinedAge=iidaAge+age))
